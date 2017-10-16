87a8db948d6e94247ec992f005a7666ad98e5350
juanrgon <jrg2156@gmail.com>
2017-10-16 15:15:48 -0400

    Better colors

--- colored-man-pages.plugin.zsh
+++ colored-man-pages.plugin.zsh
@@ -21,12 +21,12 @@ unalias man 2>/dev/null # unalias man to avoid function error
 man() {
 	env \
 		LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-		LESS_TERMCAP_md=$(printf "\e[1;31m") \
+		LESS_TERMCAP_md=$(printf "\e[1;35m") \
 		LESS_TERMCAP_me=$(printf "\e[0m") \
 		LESS_TERMCAP_se=$(printf "\e[0m") \
-		LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+		LESS_TERMCAP_so=$(printf "\e[4;36m") \
 		LESS_TERMCAP_ue=$(printf "\e[0m") \
-		LESS_TERMCAP_us=$(printf "\e[1;32m") \
+		LESS_TERMCAP_us=$(printf "\e[3;34m") \
 		PAGER="${commands[less]:-$PAGER}" \
 		_NROFF_U=1 \
 		PATH="$HOME/bin:$PATH" \
